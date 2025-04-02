e0fbfc92b87657cc6b763e43584590391b448bcb
App-Developer <rafiullahkhaleel@gmail.com>
2025-04-01 22:27:07 -0700

    commit

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_task/view/products_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), (){
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context)=>ProductsScreen()));
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset('assets/Splash.png',
+      width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height,
+
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+}
