b35ba15854770e10f84144b9346b2e475154fc58
App-Developer <rafiullahkhaleel@gmail.com>
2025-04-03 11:09:44 -0700

    commit

--- lib/view/products_screen.dart
+++ lib/view/products_screen.dart
@@ -15,6 +15,10 @@ class ProductsScreen extends StatefulWidget {
 }
 
 class _ProductsScreenState extends State<ProductsScreen> {
+  TextEditingController search = TextEditingController();
+  List<Products> dataList = [];
+  List<Products> filteredList = [];
+
   Future<ProductsModel> getProducts() async {
     final response = await http.get(
       Uri.parse('https://dummyjson.com/products?limit=100'),
@@ -28,52 +32,83 @@ class _ProductsScreenState extends State<ProductsScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    getProducts().then((data){
+        dataList = data.products!;
+        filteredList = data.products!;
+    });
+    search.addListener((){
+      filterProducts();
+    });
+  }
+
+  void filterProducts(){
+    setState(() {
+      filteredList = dataList.where((title){
+        return title.title!.toLowerCase().contains(search.text.toLowerCase());
+      }).toList();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height;
     final width = MediaQuery.sizeOf(context).width;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: FutureBuilder<ProductsModel>(
-        future: getProducts(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return SpinKitWave(color: Colors.black);
-          } else if (snapshot.hasError) {
-            return Center(child: Text('ERROR: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data == null) {
-            return Center(child: Text('No Data Available'));
-          } else {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 21),
-              child: Center(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: height*.05,
-                    ),
-                    Text('Products',style: GoogleFonts.playfairDisplay(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 24,
-                        color: Colors.black
-                    ),),
-                    SizedBox(
-                      height: height*.015,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.search,size: 30,),
-                        focusColor: Colors.black,
-                        border: OutlineInputBorder(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 21),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: height*.05,
+              ),
+              Text('Products',style: GoogleFonts.playfairDisplay(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                  color: Colors.black
+              ),),
+              SizedBox(
+                height: height*.015,
+              ),
+              SizedBox(
+                height: 45,
+                child: TextField(
+                  controller: search,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search,size: 30,),
+                      focusColor: Colors.black,
+                      border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10)
-                        )
-                      ),
-                    ),
-                    SizedBox(
-                      height: height*.02,
-                    ),
-                    Expanded(
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: height*.02,
+              ),
+              FutureBuilder<ProductsModel>(
+                future: getProducts(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Column(
+                      children: [
+                        SizedBox(
+                          height: height*.35,
+                        ),
+                        SpinKitWave(color: Colors.black),
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('ERROR: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data == null) {
+                    return Center(child: Text('No Data Available'));
+                  } else {
+                    return Expanded(
                       child: ListView.builder(
-                        itemCount: snapshot.data!.products!.length,
+                        itemCount: filteredList.length,
                         itemBuilder: (context, index) {
                           return Padding(
                             padding: const EdgeInsets.symmetric(vertical: 7),
@@ -87,7 +122,7 @@ class _ProductsScreenState extends State<ProductsScreen> {
                                   children: [
                                     CachedNetworkImage(
                                       imageUrl:
-                                          snapshot.data!.products![index].images![0]
+                                      filteredList[index].images![0]
                                               .toString(),
                                       height: height * .25,
                                       width: width * .8,
@@ -106,7 +141,7 @@ class _ProductsScreenState extends State<ProductsScreen> {
                                           children: [
                                             Expanded(
                                               child: Text(
-                                                snapshot.data!.products![index].title
+                                                filteredList[index].title
                                                     .toString(),
                                                 style: GoogleFonts.poppins(
                                                   fontSize: 14,
@@ -117,7 +152,7 @@ class _ProductsScreenState extends State<ProductsScreen> {
                                             SizedBox(width: width*.08,),
                                             Text(
                                               r'$' +
-                                                  snapshot.data!.products![index].price
+                                                  filteredList[index].price
                                                       .toString(),
                                               style: GoogleFonts.poppins(
                                                 fontSize: 16,
@@ -134,7 +169,7 @@ class _ProductsScreenState extends State<ProductsScreen> {
                                           ),
                                         ),
                                         SizedBox(height: height*.01,),
-                                        Text(snapshot.data!.products![index].brand.toString(),
+                                        Text(filteredList[index].brand.toString(),
                                           style: GoogleFonts.poppins(
                                             fontSize: 10,
                                             fontWeight: FontWeight.w400,
@@ -142,7 +177,7 @@ class _ProductsScreenState extends State<ProductsScreen> {
                                           ),
                                         ),
                                         SizedBox(height: height*.01,),
-                                        Text(snapshot.data!.products![index].category.toString(),
+                                        Text(filteredList[index].category.toString(),
                                           style: GoogleFonts.poppins(
                                               fontSize: 10,
                                               fontWeight: FontWeight.w400,
@@ -158,13 +193,13 @@ class _ProductsScreenState extends State<ProductsScreen> {
                           );
                         },
                       ),
-                    ),
-                  ],
-                ),
+                    );
+                  }
+                },
               ),
-            );
-          }
-        },
+            ],
+          ),
+        ),
       ),
     );
   }
