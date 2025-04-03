7f2c19fd30aea6ac0d2773d4d3bad3da7ec2425b
App-Developer <rafiullahkhaleel@gmail.com>
2025-04-03 04:27:07 -0700

    commit

--- lib/view/products_screen.dart
+++ lib/view/products_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_task/model/products_model.dart';
@@ -32,6 +33,7 @@ class _ProductsScreenState extends State<ProductsScreen> {
     final height = MediaQuery.sizeOf(context).height;
     final width = MediaQuery.sizeOf(context).width;
     return Scaffold(
+      backgroundColor: Colors.white,
       body: FutureBuilder<ProductsModel>(
         future: getProducts(),
         builder: (context, snapshot) {
@@ -44,52 +46,91 @@ class _ProductsScreenState extends State<ProductsScreen> {
           } else {
             return Center(
               child: ListView.builder(
-                  itemCount: snapshot.data!.products!.length,
-                  itemBuilder: (context,index){
-                    return Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        spacing: 10,
-                        children: [
-                          Image.network(snapshot.data!.products![index].images![0].toString(),
-                            height: height*.25,
-                            width: width*.8,
-                            errorBuilder: (context,error,stackTrace){
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 50),
-                              child: Center(child: Text('Image not available')),
-                            );
-                            },
-                            loadingBuilder: (context, child, loadingProgress) {
-                              if (loadingProgress == null) {
-                                return child;
-                              } else {
-                                return Center(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 100),
-                                    child: CircularProgressIndicator(),
+                itemCount: snapshot.data!.products!.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 7),
+                    child: Card(
+                      elevation: 0.7,
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Column(
+                          spacing: 10,
+                          children: [
+                            CachedNetworkImage(
+                              imageUrl:
+                                  snapshot.data!.products![index].images![0]
+                                      .toString(),
+                              height: height * .25,
+                              width: width * .8,
+                              placeholder:
+                                  (context, url) =>
+                                      SpinKitCircle(color: Colors.black),
+                              errorWidget:
+                                  (context, url, error) =>
+                                      Icon(Icons.error, color: Colors.red),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        snapshot.data!.products![index].title
+                                            .toString(),
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(width: width*.08,),
+                                    Text(
+                                      r'$' +
+                                          snapshot.data!.products![index].price
+                                              .toString(),
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  '4.9⭐⭐⭐⭐⭐',
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
                                   ),
-                                );
-                              }
-                            },
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(r'$'+snapshot.data!.products![index].title.toString(),
-                              style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w600),
-                              ),
-                              Text(r'$'+snapshot.data!.products![index].price.toString(),
-                                style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w600),
-
-                              )
-
-                            ],
-                          )
-                        ],
+                                ),
+                                SizedBox(height: height*.01,),
+                                Text(snapshot.data!.products![index].brand.toString(),
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0x800C0C0C)
+                                  ),
+                                ),
+                                SizedBox(height: height*.01,),
+                                Text(snapshot.data!.products![index].category.toString(),
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                SizedBox(height: height*.013,),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    );
-                  }),
+                    ),
+                  );
+                },
+              ),
             );
           }
         },
@@ -98,4 +139,28 @@ class _ProductsScreenState extends State<ProductsScreen> {
   }
 }
 
-
+// Image.network(
+//   snapshot.data!.products![index].images![0].toString(),
+//   height: height * .25,
+//   width: width * .8,
+//   errorBuilder: (context, error, stackTrace) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 50),
+//       child: Center(child: Text('Image not available')),
+//     );
+//   },
+//   loadingBuilder: (context, child, loadingProgress) {
+//     if (loadingProgress == null) {
+//       return child;
+//     } else {
+//       return Center(
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(
+//             vertical: 100,
+//           ),
+//           child: CircularProgressIndicator(),
+//         ),
+//       );
+//     }
+//   },
+// ),
